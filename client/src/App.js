33379e9611ff2df74e2e66186181d1d4b77ba258
Anthony Parrino <anthony.parrino@yahoo.com>
2022-01-08 00:46:20 -0500

    created logo/coin and navbar and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavComp from './components/NavComp/NavComp';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <NavComp />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/HomeComp/HomeComp.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './homeComp.css';
+
+const HomeComp = () => {
+    return (
+        <div>
+            <div className="main-container">
+                <h5>Welcome To</h5>
+                <h1 className="welcome-title">Antlantis</h1>
+                <br/>
+                <h4>A free world started from scratch. Create a society, get a job, live a long noble life..or rebel against it, change the rules and never be controlled.. the choices are yours to make. Work together and thrive or separate and suffer. Become the king or queen or slave. Create things, find valuables and get rich, build a city and fly to the moon. You directly effect the survival of the world, your neighbors and yourself.</h4>
+            </div>
+        </div>
+    )
+}
+
+export default HomeComp
+
+
+// metaverse will be a continuous world with real world threats and pleasures
+// 1. start with nothing and create a society and see where it goes -or-
+// 2. society already created and user starts out as a child to grow up and mimic life with choices to try to become rich/land a dream job
